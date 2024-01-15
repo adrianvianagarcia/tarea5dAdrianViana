1b5c9518a296bac259b3a472fbafe5bd7d0eca29
jaceara2006 <jaceara2006@g.educaand.es>
2024-01-15 10:10:22 +0100

    Añade objetos en clase main

---
+++ src/main/java/daw/main.java
@@ -0,0 +1,20 @@
+package daw;
+
+/**
+ *
+ * @author acebedo
+ */
+public class main {
+    
+    public static void main(String[] args) {
+        
+        Tarea5dAdrianViana t1 = new Tarea5dAdrianViana("Juan", 1.8, 0.5);
+        Tarea5dAdrianViana t2 = new Tarea5dAdrianViana("Pepe", 1.85, 0.4);
+        Tarea5dAdrianViana t3 = new Tarea5dAdrianViana();
+        
+        System.out.println(t1);
+        System.out.println(t2);
+        System.out.println(t3);
+    }
+    
+}
