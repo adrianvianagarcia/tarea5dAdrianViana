6a78718b6e974d2736c4014ca85479e316bc47f1
adrianviana <aviagar307@g.educaand.es>
2024-01-15 09:54:33 +0100

    Añade constructores en clase modelo

--- src/main/java/daw/Tarea5dAdrianViana.java
+++ src/main/java/daw/Tarea5dAdrianViana.java
@@ -5,4 +5,15 @@ public class Tarea5dAdrianViana {
     String nombre;
     double altura;
     double anchura;
+
+    public Tarea5dAdrianViana(String nombre, double altura, double anchura) {
+        this.nombre = nombre;
+        this.altura = altura;
+        this.anchura = anchura;
+    }
+
+    public Tarea5dAdrianViana() {
+    }
+    
+    
 }
