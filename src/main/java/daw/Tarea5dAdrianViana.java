d4aa9cb8365d6f658ee85b76cce2a1c5cb8f62c3
adrianviana <aviagar307@g.educaand.es>
2024-01-15 10:02:08 +0100

    Añadidos getters y setters

--- src/main/java/daw/Tarea5dAdrianViana.java
+++ src/main/java/daw/Tarea5dAdrianViana.java
@@ -2,9 +2,9 @@ package daw;
 
 public class Tarea5dAdrianViana {
 
-    String nombre;
-    double altura;
-    double anchura;
+    private String nombre;
+    private double altura;
+    private double anchura;
 
     public Tarea5dAdrianViana(String nombre, double altura, double anchura) {
         this.nombre = nombre;
@@ -13,6 +13,30 @@ public class Tarea5dAdrianViana {
     }
 
     public Tarea5dAdrianViana() {
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public double getAltura() {
+        return altura;
+    }
+
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    public double getAnchura() {
+        return anchura;
+    }
+
+    public void setAnchura(double anchura) {
+        this.anchura = anchura;
     }
     
     
